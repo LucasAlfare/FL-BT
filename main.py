383d54436e059ffbbf42f9d4a69367f22d393875
LucasAlfare <god-lucas@hotmail.com>
2025-06-26 19:41:05 -0300

    minimal

---
+++ main.py
@@ -0,0 +1,34 @@
+from utils import *
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import FileResponse
+from fastapi import Path
+from fastapi import BackgroundTasks
+import uuid
+
+app = FastAPI()
+
+
+@app.post('/yt-path/{url:path}')
+async def yt_path(background_tasks: BackgroundTasks, url: str = Path(...)):
+    base_dir = f'temp/{uuid.uuid4()}'
+    download_dir = f'{base_dir}/download'
+    separate_dir = f'{base_dir}/separated'
+    zip_path = f'{base_dir}/stems.zip'
+
+    os.makedirs(download_dir, exist_ok=True)
+    os.makedirs(separate_dir, exist_ok=True)
+
+    downloaded = download_youtube_audio(url, download_dir)
+    if not downloaded:
+        cleanup_path(base_dir)
+        raise HTTPException(status_code=400, detail='Download failed')
+
+    separated = separate_4stems(downloaded.output_os_path, separate_dir)
+    if not separated:
+        cleanup_path(base_dir)
+        raise HTTPException(status_code=500, detail='Separation failed')
+
+    create_zip_from_folder(separate_dir, zip_path)
+
+    background_tasks.add_task(cleanup_path, base_dir)
+    return FileResponse(zip_path, media_type='application/zip', filename='stems.zip')
