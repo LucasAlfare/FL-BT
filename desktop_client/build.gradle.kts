b4aa57d84492df3236b04d0c7c83466497eea3c8
LucasAlfare <god-lucas@hotmail.com>
2025-07-08 19:04:35 -0300

    removing desktop_client folder

--- desktop_client/build.gradle.kts
+++
@@ -1,6 +0,0 @@
-plugins {
-  alias(libs.plugins.kotlinJvm) apply false
-  alias(libs.plugins.kotlinxSerialization) apply false
-  alias(libs.plugins.compose) apply false
-  alias(libs.plugins.composeCompiler) apply false
-}
