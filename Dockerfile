d2e48719602d0ae0e33ab906c116c4c01f0713f3
LucasAlfare <god-lucas@hotmail.com>
2025-07-08 17:19:59 -0300

    split client and server builds in multiple dockerfiles

--- Dockerfile
+++ Dockerfile
@@ -7,18 +7,8 @@ ENV PYTHONDONTWRITEBYTECODE=1 \
     PIP_NO_CACHE_DIR=1
 
 RUN apt-get update && \
-    apt-get install -y openjdk-17-jdk ffmpeg && \
+    apt-get install -y ffmpeg && \
     apt-get clean
-
-WORKDIR /build
-
-COPY desktop_client /build/desktop_client
-
-WORKDIR /build/desktop_client
-RUN chmod +x ./gradlew && ./gradlew packageReleaseUberJarForCurrentOS --no-daemon
-
-RUN mkdir -p /backingtracker && \
-    cp ./app/build/compose/jars/FLBTClient-windows-x64-1.0.0-release.jar /backingtracker/client.jar
 
 WORKDIR /app
 COPY server/requirements.txt .
--- start.bat
+++ start.bat
@@ -1,13 +1,20 @@
 @echo off
 SETLOCAL
 
+IF EXIST client.jar (
+    echo [INFO] client.jar já existe, pulando build do client.
+) ELSE (
+    echo [INFO] Buildando client.jar via Docker...
+    docker build -f Dockerfile.client -t client-builder .
+    docker create --name temp-client client-builder
+    docker cp temp-client:/out/client.jar client.jar
+    docker rm temp-client
+)
+
 echo [INFO] Subindo container do backend...
 docker-compose up -d --build
 
 timeout /T 5 /NOBREAK >nul
-
-echo [INFO] Copiando .jar do container para host...
-docker cp backingtracker-server-container:/backingtracker/client.jar client.jar
 
 echo [INFO] Iniciando cliente...
 java -jar client.jar
